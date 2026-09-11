8c540aa3a428a9cf52fa37783cb432e8ffdfea27
Gero Konberg <gerokonberg@proton.me>
2026-09-11 22:41:14 +0200

    Music data pages

---
+++ snes/b03.asm
@@ -0,0 +1,16 @@
+;BANK 03
+
+Song01:
+	incbin "audio/01.mid"
+Song02:
+	incbin "audio/02.mid"
+Song03:
+	incbin "audio/03.mid"
+Song04:
+	incbin "audio/04.mid"
+Song05:
+	incbin "audio/05.mid"
+Song06:
+	incbin "audio/06.mid"
+
+;	db "BANK03"
---
+++ snes/b04.asm
@@ -0,0 +1,12 @@
+;BANK 04
+
+Song07:
+	incbin "audio/07.mid"
+Song08:
+	incbin "audio/08.mid"
+Song09:
+	incbin "audio/09.mid"
+Song0A:
+	incbin "audio/0A.mid"
+
+;	db "BANK04"
